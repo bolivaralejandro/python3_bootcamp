f43545e08aecc23dd30507af3f45a6ca31870ec9
Alejandro Bolivar <abolivar@abolivar-ltm1.internal.salesforce.com>
2022-10-04 21:19:04 -0400

    edited v3

--- v3.py
+++ v3.py
@@ -1,13 +1,35 @@
-p1 = input("Player 1: rock, paper, or scissors ")
-p2 = input("Player 2: rock, paper, or scissors ")
-     
-if p1 == p2:
-    print("Draw")
-elif p1 == "rock" and p2 == "scissors":
-    print("p1 wins")
-elif p1 == "paper" and p2 == "rock":
-    print("p1 wins")
-elif p1 == "scissors" and p2 == "paper":
-    print("p1 wins")
+from random import randint
+# print("Rock...")
+# print("Paper...")
+# print("Scissors...")
+
+player = input("Player, make your move: ").lower()
+rand_num = randint(0,2)
+if rand_num == 0:
+	computer = "rock"
+elif rand_num == 1:
+	computer = "paper"
 else:
-    print("p2 wins")
+	computer = "scissors"
+
+print(f"Computer plays {computer}" )
+
+if player == computer:
+	print("It's a tie!")
+elif player == "rock":
+	if computer == "scissors":
+		print("player wins!")
+	else:
+		print("computer wins!")
+elif player == "paper":
+	if computer == "rock":
+		print("player wins!")
+	else:
+		print("computer wins!")
+elif player == "scissors":
+	if computer == "paper":
+		print("player wins!")
+	else:
+		print("computer wins!")	
+else:
+	print("Please enter a valid move!")
