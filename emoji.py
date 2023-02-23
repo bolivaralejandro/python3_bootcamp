87a04a4a0afb1a014dcd5752703d6ae688183f8b
Alejandro Bolivar <bolivaral@gmail.com>
2023-02-22 21:34:15 -0500

    added files

--- emoji.py
+++ emoji.py
@@ -1,7 +1,7 @@
-# for num in range(1, 10):
-#     print("\U0001f600" * num)
+for num in range(1, 10):
+	print("\U0001f600" * num)
 
 times = 1
 while times < 11:
-    print("\U0001f600" * num
-    times +=1
+	print("\U0001f600" * num)
+	times +=1
